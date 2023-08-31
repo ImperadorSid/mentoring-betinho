1f9440b91e623f223044c31c78d8b5aefb98cb00
= Moisés Carvalho <moisescarv27@hotmail.com>
2023-08-31 12:39:30 -0300

    Add new tests for removing bets and submitting form

--- src/components/__tests__/PlaceBet.js
+++ src/components/__tests__/PlaceBet.js
@@ -28,21 +28,37 @@ describe('<PlaceBet />', () => {
     describe('without stake', () => {
       const selectedBet = create('bet')
 
-      beforeEach(() => setup({ selectedBet }))
-
       it('shows selected event category', () => {
+        setup({ selectedBet })
+
         expect(screen.getByText('Soccer')).toBeInTheDocument()
       })
 
       it('shows selected event name', () => {
+        setup({ selectedBet })
+
         expect(screen.getByText('Real Madrid vs Barcelona')).toBeInTheDocument()
       })
 
       it('shows selected competitor name', () => {
+        setup({ selectedBet })
+
         expect(screen.getByText('Real Madrid')).toBeInTheDocument()
       })
 
-      it('should disable "Place Bet" button', () => {
+      it('shows call removeBet callback when the "Remove" link is clicked', async () => {
+        const removeBet = jest.fn()
+        setup({ selectedBet, removeBet })
+        const removeButton = screen.getByText('x Remove')
+
+        await userInteration.click(removeButton)
+
+        expect(removeBet).toBeCalled()
+      })
+
+      it('should the submit button be disabled', () => {
+        setup({ selectedBet })
+
         expect(screen.getByRole('button', { value: 'Place Bet' })).toBeDisabled()
       })
     })
@@ -62,13 +78,7 @@ describe('<PlaceBet />', () => {
         expect(screen.getByText('28.00')).toBeInTheDocument()
       })
 
-      it('should enable "Place Bet" button', () => {
-        setup({ selectedBet })
-
-        expect(screen.getByRole('button', { value: 'Place Bet' })).toBeEnabled()
-      })
-
-      it('should call function after change stake in input field', async () => {
+      it('should update stake when something is typed in the input field', async () => {
         const changeBetStake = jest.fn()
         setup({ selectedBet, changeBetStake })
         const stakeInput = screen.getByRole('textbox')
@@ -78,6 +88,73 @@ describe('<PlaceBet />', () => {
 
         expect(changeBetStake).toBeCalled()
       })
+
+      it('should submit button be enabled', () => {
+        setup({ selectedBet })
+
+        expect(screen.getByRole('button', { value: 'Place Bet' })).toBeEnabled()
+      })
+
+      it('should submit bet event data when the button is pressed', async () => {
+        const placeBet = jest.fn()
+        setup({ selectedBet, placeBet })
+        const submitButton = screen.getByRole('button', { value: 'Place Bet' })
+
+        await userInteration.click(submitButton)
+
+        expect(placeBet).toBeCalled()
+      })
+    })
+  })
+
+  describe('there are multiple bets selected', () => {
+    const selectedBet = create('bet', {
+      events: [
+        { id: 1, competitorId: 1, odd: 2.8, categoryId: 1 },
+        { id: 2, competitorId: 4, odd: 0.8, categoryId: 2 },
+      ],
+    })
+
+    it('shows selected categories', () => {
+      setup({ selectedBet })
+
+      expect(screen.getByText('Soccer')).toBeInTheDocument()
+      expect(screen.getByText('Basketball')).toBeInTheDocument()
+    })
+
+    it('shows selected events names', () => {
+      setup({ selectedBet })
+
+      expect(screen.getByText('Real Madrid vs Barcelona')).toBeInTheDocument()
+      expect(screen.getByText('Rivers vs Eagles')).toBeInTheDocument()
+    })
+
+    it('shows selected competitors name', () => {
+      setup({ selectedBet })
+
+      expect(screen.getByText('Real Madrid')).toBeInTheDocument()
+      expect(screen.getByText('Eagles')).toBeInTheDocument()
+    })
+
+    it('shows call to remove single event from selection', async () => {
+      const removeBetEvent = jest.fn()
+      setup({ selectedBet, removeBetEvent })
+      const removeButtons = screen.getAllByTitle('Remove this bet')
+
+      await userInteration.click(removeButtons[0])
+      await userInteration.click(removeButtons[1])
+
+      expect(removeBetEvent).toBeCalledTimes(2)
+    })
+
+    it('shows call removeBet callback when the "Remove All" link is clicked', async () => {
+      const removeBet = jest.fn()
+      setup({ selectedBet, removeBet })
+      const removeButton = screen.getByText('x Remove All')
+
+      await userInteration.click(removeButton)
+
+      expect(removeBet).toBeCalled()
     })
   })
 })
